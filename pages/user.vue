ccf0d7a6b44dfb0b73fb27ced2389e32f19a9a40
Mutasim Billah <mutasim.wre@gmail.com>
2024-01-30 14:38:01 +0600

    minor modification

--- pages/user.vue
+++ pages/user.vue
@@ -21,7 +21,11 @@ const
   <v-container class="h-100">
     <v-row class="h-100">
       <v-col class="d-flex justify-center align-center">
-        <pre>{{ user }}</pre>
+        <v-card>
+          <v-card-text>
+            Welcome to Fall Armyworm Monitor admin dashboard
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
